1c7ea9dd3b86809d9a711a984e86951a6e6a13f9
Brajjaan <bryanvouriot@hotmail.com>
2024-11-11 15:54:48 +0100

    4

---
+++ YellowBelt/_4Kata6/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _4Kata6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] enemies = ["Goblin", "Orc", "Troll", "Skeleton", "Dragon"];
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            Console.WriteLine(enemies[i]);
+        }
+        Console.WriteLine();
+
+        List<string> inventory = new List<string>
+        {
+            "Sword",
+            "Shield",
+            "Potion"
+        };
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            Console.WriteLine(inventory[i]);
+        }
+        Console.WriteLine();
+        inventory.Add("Helmet");
+        inventory.Add("Armor");
+        inventory.Remove("Potion");
+
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            Console.WriteLine(inventory[i]);
+        }
+
+        Console.WriteLine("Number of items in inventory: " + inventory.Count);
+    }
+}
