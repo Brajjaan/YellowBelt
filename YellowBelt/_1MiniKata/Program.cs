4b2c490df26c60e79ac1d74aafda792d81cba93e
Brajjaan <bryanvouriot@hotmail.com>
2024-11-11 10:34:22 +0100

    1

---
+++ YellowBelt/_1MiniKata/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1MiniKata;
+
+class Program
+{    
+    public int Damage = 15; 
+    public int HealRate = 20;
+    
+    static void Main(string[] args)
+    {
+        Attack(15);
+        Heal(15);
+    }
+    public static void Attack(int damage)
+    {
+        Console.WriteLine($"Player dealt {damage} damage!");
+    }
+
+    public static void Heal(int heal)
+    {
+        Console.WriteLine($"Player healed for {heal} hp!");
+    }
+}
