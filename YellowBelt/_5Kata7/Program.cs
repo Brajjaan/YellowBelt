b570689b48a3cff385171d33a9db24735155af5e
Brajjaan <bryanvouriot@hotmail.com>
2024-11-12 08:04:42 +0100

    5

---
+++ YellowBelt/_5Kata7/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _5Kata7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new();
+        Enemy enemy = new();
+
+        player.Name = "Arin";
+        player.Health = 100;
+        player.Level = 1;
+        enemy.Type = "Goblin";
+        enemy.Health = 50;
+        enemy.Damage = 10;
+        
+        Console.WriteLine($"Player Info: \nName: {player.Name} \nHealth: {player.Health} \nLevel: {player.Level}");
+        Console.WriteLine();
+        Console.WriteLine($"Enemy Info \nType: {enemy.Type} \nHealth: {enemy.Health} \nDamage: {enemy.Damage}");
+
+    }
+}
+
+class Player
+{
+    public string Name;
+    public int Health;
+    public int Level;
+}
+
+class Enemy
+{
+    public string Type;
+    public int Health;
+    public int Damage;
+}
